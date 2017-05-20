742e2da7b1b0b42e5c0fb48dc0d2d58a4bae0c4d
Eloy Gomez <eloy@indeos.es>
2017-05-20 13:46:25 +0200

    Don't start fluent_lager if not configured

--- lib/fluent_lager.ex
+++ lib/fluent_lager.ex
@@ -3,12 +3,21 @@ defmodule FluentLager do
 
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
-    GenEvent.add_handler(:lager_event, FluentLager.EventHandler, [])
+
+    add_event_handler()
+
     children = [
       worker(FluentLager.Fluent, [])
     ]
 
     opts = [strategy: :one_for_one, name: FluentLager.Supervisor]
     Supervisor.start_link(children, opts)
+  end
+
+
+  defp add_event_handler do
+    if Application.get_env(:fluent_lager, :tag) != nil do
+      GenEvent.add_handler(:lager_event, FluentLager.EventHandler, [])
+    end
   end
 end
